ae9f5f954be741b44cd8b54162d31e3ec936e21d
TouchFriend <1604181058@qq.com>
2025-09-01 22:07:33 +0800

    反反调试sysctl

--- BiliBiliMDDylib/AntiAntiDebug/AntiAntiDebug.m
+++ BiliBiliMDDylib/AntiAntiDebug/AntiAntiDebug.m
@@ -93,7 +93,7 @@ int my_syscall(int code, va_list args){
     return orig_syscall(code, newArgs);
 }
 
-__attribute__((constructor)) static void entry(){
+__attribute__((constructor)) static void entry(void){
     NSLog(@"[AntiAntiDebug Init]");
     
     rebind_symbols((struct rebinding[1]){{"ptrace", my_ptrace, (void*)&orig_ptrace}},1);
@@ -101,7 +101,7 @@ __attribute__((constructor)) static void entry(){
 //    rebind_symbols((struct rebinding[1]){{"dlsym", my_dlsym, (void*)&orig_dlsym}},1);
     
     //some app will crash with _dyld_debugger_notification
-    // rebind_symbols((struct rebinding[1]){{"sysctl", my_sysctl, (void*)&orig_sysctl}},1);
+    rebind_symbols((struct rebinding[1]){{"sysctl", my_sysctl, (void*)&orig_sysctl}},1);
     
     rebind_symbols((struct rebinding[1]){{"syscall", my_syscall, (void*)&orig_syscall}},1);
 }
